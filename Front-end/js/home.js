fb34e370e5bece940aee5330722a31cda6a0ca25
fabio.vinicios <fabio.vinicios@totvs.com.br>
2025-06-14 17:30:20 -0300

    Implementação do Modal de unidades

---
+++ Front-end/js/home.js
@@ -0,0 +1,7 @@
+function direcWhatsapp() {
+  if($('#stUnidade').val()=='Pirituba'){
+    window.location.href = "https://api.whatsapp.com/send?phone=551139011063&text=Olá, gostaria de saber mais referente a mutvet!";
+  }else{
+    window.location.href = "https://api.whatsapp.com/send?phone=551139011063&text=Olá, gostaria de saber mais referente a mutvet!";
+  }
+}

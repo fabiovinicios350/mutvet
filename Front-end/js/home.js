0d7d274c962415903662dc4e809e2b470bc61d03
Fábio Vinicios <56418109+fabiovinicios350@users.noreply.github.com>
2026-02-18 16:05:08 -0300

    Update home.js
    Ajuste de telefone unidade pirituba

--- Front-end/js/home.js
+++ Front-end/js/home.js
@@ -1,6 +1,6 @@
 function direcWhatsapp() {
   if($('#stUnidade').val()=='Pirituba'){
-    window.location.href = "https://api.whatsapp.com/send?phone=5511975112048&text=Olá, gostaria de saber mais referente a mutvet!";
+    window.location.href = "https://api.whatsapp.com/send?phone=5511948550693&text=Olá, gostaria de saber mais referente a mutvet!";
   }else{
     window.location.href = "https://api.whatsapp.com/send?phone=5511987869253&text=Olá, gostaria de saber mais referente a mutvet!";
   }
